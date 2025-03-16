ca593e6143470030fefff4b94945466bdaa9eade
miggui rivera <migguirivera@gmail.com>
2025-03-16 20:33:57 +0800

    test

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -451,4 +451,4 @@ def get_all_Budget():
     return jsonify(list(ordinances_dict.values()))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)

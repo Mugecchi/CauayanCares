50b45cc08e11607852f6a6d9a54a2e493351d598
miggui rivera <migguirivera@gmail.com>
2025-03-16 19:24:30 +0800

    deployment test

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -451,4 +451,5 @@ def get_all_Budget():
     return jsonify(list(ordinances_dict.values()))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)

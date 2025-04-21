1a9af21be77e9fc07596048d2dfaa87b39849db7
Mugecchi <migguirivera@gmail.com>
2025-04-21 11:55:19 +0800

    test

--- BackEnd/db.py
+++ BackEnd/db.py
@@ -8,7 +8,7 @@ def get_db_connection():
             user=os.getenv("MYSQLUSER"),
             password=os.getenv("MYSQLPASSWORD"),
             database=os.getenv("MYSQLDATABASE"),
-            port=int(os.getenv("MYSQLPORT", 3306))
+            port=int(os.getenv("MYSQLPORT"))
         )
     else:
         return mysql.connector.connect(
